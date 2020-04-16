247b52cc3d3a5cfb1167451c5755cc8dcd1a2dd3
jeff perreton <jperreton@gmail.com>
2020-04-17 01:59:22 +0200

    u

--- R/main.R
+++ R/main.R
@@ -1,6 +1,6 @@
 #' @import sparklyr
 #' @export
 sparklyudf_register <- function(sc, schemaRegistryUrl) {
-  sparklyr::invoke_static(sc, "sparklyudf.Main", "deserialize", spark_session(sc), schemaRegistryUrl)
-  sparklyr::invoke_static(sc, "sparklyudf.Main", "getSchema", spark_session(sc), schemaRegistryUrl)
+  sparklyr::invoke_static(sc, "sparklyudf.Main", "register_deserialize", spark_session(sc), schemaRegistryUrl)
+  sparklyr::invoke_static(sc, "sparklyudf.Main", "register_getSchema", spark_session(sc), schemaRegistryUrl)
 }
