a8d306b900d1c5318b4c2781089429836dbf3fae
jeff perreton <jperreton@gmail.com>
2020-04-16 19:12:52 +0200

    u

--- R/dependencies.R
+++ R/dependencies.R
@@ -6,7 +6,9 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         package = "sparklyudf"
       )
     ),
-    packages = c("org.apache.spark:spark-avro_2.11:2.4.5", "org.apache.avro:avro:1.9.2", "io.confluent:kafka-avro-serializer:5.4.1", "io.confluent:kafka-schema-registry-client:5.4.1")
+    packages = c("org.apache.spark:spark-avro_2.11:2.4.5", "org.apache.avro:avro:1.9.2", 
+	            "kafka-schema-registry-5.4.1.kafka-avro-serializer:5.4.1", "io.confluent:kafka-schema-registry-client:5.4.1",
+				"io.confluent:afka-schema-registry:5.4.1")
   )
 }
 
