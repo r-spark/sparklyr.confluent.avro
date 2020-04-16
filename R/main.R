f5a11a639dae2ff07d98a1ebaf7807d4160432e2
jeff perreton <jperreton@gmail.com>
2020-04-16 16:57:30 +0200

    dependencies

--- R/main.R
+++ R/main.R
@@ -2,4 +2,5 @@
 #' @export
 sparklyudf_register <- function(sc) {
   sparklyr::invoke_static(sc, "sparklyudf.Main", "create_deserializer", spark_session(sc))
+  sparklyr::invoke_static(sc, "sparklyudf.Main", "getAvroSchema", spark_session(sc))
 }
