0606daa8b2b77331e0ec5c55e3ed6615686b3ec6
jeff perreton <jperreton@gmail.com>
2020-04-16 10:02:22 +0200

    avro prototype

--- java/main.scala
+++ java/main.scala
@@ -1,11 +1,43 @@
 package sparklyudf
 
-import org.apache.spark.sql.SparkSession;
+import com.databricks.spark.avro.SchemaConverters
+import io.confluent.kafka.schemaregistry.client.{CachedSchemaRegistryClient, SchemaRegistryClient}
+import io.confluent.kafka.serializers.AbstractKafkaAvroDeserializer
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericRecord
+import org.apache.spark.sql.SparkSession
 
 object Main {
-  def register_hello(spark: SparkSession) = {
-    spark.udf.register("hello", (name: String) => {
-      "Hello, " + name + "! - From Scala"
-    })
+  class AvroDeserializer extends AbstractKafkaAvroDeserializer {
+    def this(client: SchemaRegistryClient) {
+      this()
+      this.schemaRegistry = client
+    }
+
+    override def deserialize(bytes: Array[Byte]): String = {
+      val genericRecord = super.deserialize(bytes).asInstanceOf[GenericRecord]
+      genericRecord.toString
+    }
   }
+  
+  def create_deserializer(spark: SparkSession, kafkaUrl: String, schemaRegistryUrl: String): object = {
+    val schemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryUrl, 128)
+    val kafkaAvroDeserializer = new AvroDeserializer(schemaRegistryClient)
+	object DeserializerWrapper {
+	  val client=schemaRegistryClient
+      val deserializer = kafkaAvroDeserializer
+	  def getSchema(topic: String) {
+	    val avroSchema = schemaRegistryClient.getLatestSchemaMetadata(topic + "-value").getSchema
+        SchemaConverters.toSqlType(new Schema.Parser().parse(avroSchema))
+	  }
+    }
+	spark.udf.register("deserialize", (bytes: Array[Byte]) =>
+      DeserializerWrapper.deserializer.deserialize(bytes)
+    )
+	spark.udf.register("getAvroSchema", (topic: String) =>
+      DeserializerWrapper.getSchema(topic)
+    )
+	
+  }
+  
 }
