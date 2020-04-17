fdd5db491cb55d970c4523343679d72a324af14b
jeff perreton <jperreton@gmail.com>
2020-04-17 20:20:06 +0200

    u

--- java/main.scala
+++ java/main.scala
@@ -1,43 +1,19 @@
 package sparklyudf
 
-import org.apache.spark.sql.avro.SchemaConverters
-import io.confluent.kafka.schemaregistry.client.{CachedSchemaRegistryClient, SchemaRegistryClient}
-import io.confluent.kafka.serializers.AbstractKafkaAvroDeserializer
-import org.apache.avro.Schema
-import org.apache.avro.generic.GenericRecord
-import org.apache.spark.sql.SparkSession
+import com.hortonworks.spark.registry.util._
+import org.apache.spark.sql.Column
 
 object Main {
-  class AvroDeserializer extends AbstractKafkaAvroDeserializer {
-    def this(client: SchemaRegistryClient) {
-      this()
-      this.schemaRegistry = client
-    }
-
-    override def deserialize(bytes: Array[Byte]): String = {
-      val genericRecord = super.deserialize(bytes).asInstanceOf[GenericRecord]
-      genericRecord.toString
-    }
-  }
-  val schemaRegistryUrl="http://schema-registry:8081"
-  val schemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryUrl, 128)
-  val kafkaAvroDeserializer = new AvroDeserializer(schemaRegistryClient)
-  object DeserializerWrapper {
-	  val client=schemaRegistryClient
-      val deserializer = kafkaAvroDeserializer
-  }
+  val schemaRegistryUrl = "http://schema-registry:8081"
+  import spark.implicits._
+  val config = Map[String, Object]("schema.registry.url" -> schemaRegistryUrl)
+  implicit val srConfig: SchemaRegistryConfig = SchemaRegistryConfig(config)
   
   def register_deserialize(spark: SparkSession, schemaRegistryUrl: String) = {
-	spark.udf.register("deserialize", (bytes: Array[Byte]) => {
-      DeserializerWrapper.deserializer.deserialize(bytes)
+	spark.udf.register("deserialize", (data: Column, topic: String) => {
+      from_sr(bytes, topic)
 	  }
     )
-  }
-  def register_getSchema(spark: SparkSession) = {
-	spark.udf.register("getSchema", (topic: String) => {
-	  schemaRegistryClient.getLatestSchemaMetadata(topic + "-value").getSchema
-	  }
-	)
   }
   
 }
