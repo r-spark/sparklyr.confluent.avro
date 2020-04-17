f9e794e7524b80d431433f2d37c60635060f1736
jeff perreton <jperreton@gmail.com>
2020-04-17 11:59:07 +0200

    u

--- R/dependencies.R
+++ R/dependencies.R
@@ -8,7 +8,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
     ),
     packages = c("org.apache.spark:spark-avro_2.11:2.4.5", "org.apache.avro:avro:1.9.2", 
 	            "io.confluent:kafka-avro-serializer:5.4.1",
-				"io.confluent:kafka-schema-registry-client:5.4.1",
+				"io.confluent:kafka-schema-registry-client:5.4.1", "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5",
 				"io.confluent:kafka-schema-registry:5.4.1", "org.apache.kafka:kafka-clients:5.4.1-ce"),
 	repositories=c("http://central.maven.org/maven2/", "http://packages.confluent.io/maven/")
   )
