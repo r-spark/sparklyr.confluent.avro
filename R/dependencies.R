a0c8dc5368dab3b4f59948afc4c296d220982731
jeff perreton <jperreton@gmail.com>
2020-04-18 16:24:54 +0200

    u

--- R/dependencies.R
+++ R/dependencies.R
@@ -6,10 +6,14 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         package = "sparklyudf"
       ), 
 	  system.file(
-        "java/sparklyudf-2.4-5_2.11.jar",
+        "java/spark-schema-registry-0.1-SNAPSHOT-jar-with-dependencies.jar",
+        package = "sparklyudf"
+      ),
+	  
+	  system.file(
+        "java/spark-sql-kafka-0-10_2.11-2.4.5.jar",
         package = "sparklyudf"
       )
-	  
     )
   )
 }
