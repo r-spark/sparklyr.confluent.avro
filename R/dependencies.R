66e763cfb5281b5a160e85a40cd3b3f7932161d9
jeff perreton <jperreton@gmail.com>
2020-04-18 15:56:07 +0200

    u

--- R/dependencies.R
+++ R/dependencies.R
@@ -5,7 +5,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         "java/sparklyudf-2.4-5_2.11.jar",
         package = "sparklyudf"
       )
-    )
+    ),
     packages = c("com.hortonworks:spark-schema-registry:0.1-SNAPSHOT", "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5")
   )
 }
