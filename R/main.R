0c1be9f1dfb7415507ad49fb0520dd2e112e4771
jeff perreton <jperreton@gmail.com>
2020-04-18 14:20:28 +0200

    u

--- R/main.R
+++ R/main.R
@@ -1,5 +1,5 @@
 #' @import sparklyr
 #' @export
 sparklyudf_register <- function(sc) {
-  sparklyr::invoke_static(sc, "sparklyudf.Main", "register_deserialize", spark_session(sc)))
+  sparklyr::invoke_static(sc, "sparklyudf.Main", "register_deserialize", spark_session(sc))
 }
