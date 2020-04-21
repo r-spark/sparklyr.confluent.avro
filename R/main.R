0b64da9a9921ac16d2e3e69aabd921807ca27543
jeff perreton <jperreton@gmail.com>
2020-04-21 14:45:59 +0200

    scala package name

--- R/main.R
+++ R/main.R
@@ -2,5 +2,5 @@
 #' @export
 stream_read_kafka_avro <- function(sc, topic, master= "local[*]", startingOffsets="latest", kafkaUrl= "broker:9092",
              schemaRegistryUrl= "http://schema-registry:8081", logLevel= "ERROR", jobName="sample") {
-  invoke_static(sc, "sparklyr_confluent_avro.Reader", "stream", topic, master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName)
+  invoke_static(sc, "sparklyr.confluent.avro.Reader", "stream", topic, master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName)
 }
