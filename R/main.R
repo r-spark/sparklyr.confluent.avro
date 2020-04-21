f7e50abc883d0c44e026e910b7920005993c3d74
jeff perreton <jperreton@gmail.com>
2020-04-21 12:29:17 +0200

    u

--- R/main.R
+++ R/main.R
@@ -1,6 +1,6 @@
 #' @import sparklyr
 #' @export
 stream_read_kafka_avro <- function(sc, topic, master= "local[*]", startingOffsets="latest", kafkaUrl= "broker:9092",
-             schemaRegistryUrl= "http://schema-registry:8081", logLevel= "ERROR", jobName=String = "sample") {
+             schemaRegistryUrl= "http://schema-registry:8081", logLevel= "ERROR", jobName="sample") {
   invoke_static(sc, "sparklyudf.Reader", "stream", topic, master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName)
 }
