bad91d58ade06ee914f30f9922272b702204dad5
jeff perreton <jperreton@gmail.com>
2020-04-21 15:14:31 +0200

    package name

--- R/dependencies.R
+++ R/dependencies.R
@@ -3,7 +3,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
     jars =
       system.file(
         "java/sparklyr.confluent.avro-2.4-5_2.11.jar",
-        package = "sparklyudf"
+        package = "sparklyr.confluent.avro"
       ),
 	packages="io.confluent:kafka-schema-registry:5.4.1,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,org.apache.spark:spark-avro_2.11:2.4.5,za.co.absa:abris_2.11:3.1.1",
     repositories="http://packages.confluent.io/maven/"
