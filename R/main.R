feb230e484cb9c1a18edc66e44565b4e97b6cb9b
jeff perreton <jperreton@gmail.com>
2020-04-27 14:52:07 +0200

    adding support for writing avro

--- R/main.R
+++ R/main.R
@@ -2,5 +2,11 @@
 #' @export
 stream_read_kafka_avro <- function(sc, topic, master= "local[*]", startingOffsets="latest", kafkaUrl= "broker:9092",
              schemaRegistryUrl= "http://schema-registry:8081", logLevel= "ERROR", jobName="sample") {
-  invoke_static(sc, "sparklyr.confluent.avro.Reader", "stream", topic, master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName)
+  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_read", topic, master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName)
+}
+
+stream_write_kafka_avro <- function(sc, topic, dataFrame, structName, schemaRegistryUrl="http://schema-registry:8081",
+                   valueSchemaNamingStrategy= "value.schema.naming.strategy", avroRecordName="avro.record.name",
+				   avroRecordNamespace: String = "avro.record.namespace") {
+  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame, structName, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace)
 }
--- java/main.scala
+++ java/main.scala
@@ -7,10 +7,11 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import za.co.absa.abris.examples.utils.ExamplesUtils._
 import org.apache.spark.sql.SparkSession
 import za.co.absa.abris.avro.functions.from_confluent_avro
+import za.co.absa.abris.avro.functions.to_confluent_avro
 
-object Reader {
+object Bridge {
   
-  def stream(topic: String, master: String = "local[*]", startingOffsets: String = "latest", kafkaUrl: String = "broker:9092",
+  def stream_read(topic: String, master: String = "local[*]", startingOffsets: String = "latest", kafkaUrl: String = "broker:9092",
              schemaRegistryUrl: String = "http://schema-registry:8081", logLevel: String = "ERROR", jobName: String = "sample") = {
     val properties = new Properties()
 	properties.setProperty("job.name", jobName)
@@ -29,5 +30,18 @@ object Reader {
 	val schemaRegistryConfig = properties.getSchemaRegistryConfigurations("option.subscribe")
     val stream = spark.readStream.format("kafka").option("startingOffsets", startingOffsets).option("kafka.bootstrap.servers", kafkaUrl).addOptions(properties)
     stream.load().select(from_confluent_avro(col("value"), schemaRegistryConfig) as 'value)
+  }
+  
+  def stream_write(topic: String, dataFrame: Dataset[Row], structName: String, schemaRegistryUrl: String = "http://schema-registry:8081",
+                   valueSchemaNamingStrategy: String = "value.schema.naming.strategy", avroRecordName: String = "avro.record.name",
+				   avroRecordNamespace: String = "avro.record.namespace") = {
+    val registryConfig = Map(
+      "schema.registry.topic" -> topic,
+      "schema.registry.url" -> schemaRegistryUrl,
+      "value.schema.naming.strategy" -> valueSchemaNamingStrategy,
+      "schema.name" -> avroRecordName,
+      "schema.namespace"-> avroRecordNamespace
+      )
+    dataFrame.select(to_confluent_avro(col(structName), registryConfig) as 'value)
   }
 }
