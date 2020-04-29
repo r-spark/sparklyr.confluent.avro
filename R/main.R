a35a6ac2d6549a16ce36e4a379afa458ac0fd2ef
jeff perreton <jperreton@gmail.com>
2020-04-29 20:07:16 +0200

    remove default params

--- R/main.R
+++ R/main.R
@@ -1,11 +1,11 @@
 #' @import sparklyr
 #' @export
-stream_read_kafka_avro <- function(sc, topic, master= "local[*]", startingOffsets="latest", kafkaUrl= "broker:9092",
-             schemaRegistryUrl= "http://schema-registry:8081", logLevel= "ERROR", jobName="sample") {
+stream_read_kafka_avro <- function(sc, topic, master= "local[*]", startingOffsets="latest", kafkaUrl,
+             schemaRegistryUrl, logLevel= "ERROR", jobName="sample") {
   invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_read", topic, master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName)
 }
 
-stream_write_kafka_avro <- function(sc, topic, dataFrame, kafkaUrl= "broker:9092", schemaRegistryUrl="http://schema-registry:8081",
+stream_write_kafka_avro <- function(sc, topic, dataFrame, kafkaUrl, schemaRegistryUrl,
                    valueSchemaNamingStrategy= "topic.name", avroRecordName="RecordName",
 				   avroRecordNamespace= "RecordNamespace", checkPointLocation="a") {
   invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame, kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
