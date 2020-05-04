69e2f656bd67ea8dd46c69450f2256fc9b3670d4
jeff perreton <jperreton@gmail.com>
2020-05-04 22:52:00 +0200

    remove value when reading

--- R/main.R
+++ R/main.R
@@ -14,6 +14,5 @@ stream_read_kafka_avro <- function(sc, topic, master= "local[*]", startingOffset
 stream_write_kafka_avro <- function(sc, topic, dataFrame, kafkaUrl, schemaRegistryUrl,
                    valueSchemaNamingStrategy= "topic.name", avroRecordName="RecordName",
 				   avroRecordNamespace= "RecordNamespace", checkPointLocation="a") {
-  dataFrame %>%
-  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame=., kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
+  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame=dataFrame, kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
 }
