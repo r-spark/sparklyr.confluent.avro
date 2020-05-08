0e6b879e4fcd5541d42ada79ebc9c1c9db743925
jeff perreton <jperreton@gmail.com>
2020-05-08 11:49:08 +0200

    remove master

--- R/main.R
+++ R/main.R
@@ -14,7 +14,5 @@ stream_read_kafka_avro <- function(sc, topic, startingOffsets="latest", kafkaUrl
 stream_write_kafka_avro <- function(sc, topic, dataFrame, kafkaUrl, schemaRegistryUrl,
                    valueSchemaNamingStrategy= "topic.name", avroRecordName="RecordName",
 				   avroRecordNamespace= "RecordNamespace", checkPointLocation="a") {
-  t <- dataFrame %>% spark_dataframe()
-  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame=t, kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
-  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame=t, kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
+  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame=dataFrame %>% spark_dataframe(), kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
 }
