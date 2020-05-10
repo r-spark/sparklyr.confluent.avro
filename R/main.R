bffd57511b5500b898d122b54efb9117fd584709
jeff perreton <jperreton@gmail.com>
2020-05-10 14:53:23 +0200

    simplify invoke

--- R/main.R
+++ R/main.R
@@ -1,18 +1,38 @@
 #' @import sparklyr
 #' @export
-stream_read_kafka_avro <- function(sc, topic, startingOffsets="latest", kafkaUrl,
-             schemaRegistryUrl, logLevel= "ERROR", jobName="sample", name=NULL) {
-  if(is.null(name)) {
+stream_read_kafka_avro <- function (sc, kafka.bootstrap.servers, schema.registry.topic, startingOffsets, 
+                                    schema.registry.url, value.schema.naming.strategy, value.schema.id, name = NULL, ...) {
+ if(is.null(name)) {
     name <- topic
   }
-  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_read", topic, sc$master, startingOffsets, kafkaUrl, schemaRegistryUrl, logLevel, jobName) %>%
-  invoke("select", "value.*", list()) %>% 
+  registryConfig <- new.env()
+  registryConfig$schema.registry.topic <- schema.registry.topic
+  registryConfig$schema.registry.url <- schema.registry.url
+  registryConfig$value.schema.naming.strategy <- value.schema.naming.strategy
+  registryConfig$value.schema.id <- value.schema.id
+  stream_read_kafka(sc, options=list(kafka.bootstrap.servers = kafka.bootstrap.servers, subscribe = schema.registry.topic, startingOffsets=startingOffsets)) %>%
+  spark_dataframe() %>%
+  invoke("select", list(invoke(invoke_static(sc, "za.co.absa.abris.avro.functions", "from_confluent_avro", invoke_static(sc, "org.apache.spark.sql.functions", "col", "value"), registryConfig), "as", "value"))) %>%
+  invoke("select", "value.*", list())%>%
   invoke("createOrReplaceTempView", name)
   tbl(sc, name)
 }
 
-stream_write_kafka_avro <- function(sc, topic, dataFrame, kafkaUrl, schemaRegistryUrl,
-                   valueSchemaNamingStrategy= "topic.name", avroRecordName="RecordName",
-				   avroRecordNamespace= "RecordNamespace", checkPointLocation="a") {
-  invoke_static(sc, "sparklyr.confluent.avro.Bridge", "stream_write", topic, dataFrame=dataFrame %>% spark_dataframe(), kafkaUrl, schemaRegistryUrl, valueSchemaNamingStrategy, avroRecordName,avroRecordNamespace, checkPointLocation)
+#' @import sparklyr
+#' @export
+stream_write_kafka_avro <- function (x, mode = c("append", "complete", "update"), trigger = stream_trigger_interval(), 
+    checkpoint = file.path("checkpoints", random_string("")), kafka.bootstrap.servers, schema.registry.topic, schema.registry.url, 
+	value.schema.naming.strategy, schema.name, schema.namespace,
+    options = list(), ...) {
+  registryConfig <- new.env()
+  registryConfig$schema.registry.topic <- schema.registry.topic
+  registryConfig$schema.registry.url <- schema.registry.url
+  registryConfig$value.schema.naming.strategy <- value.schema.naming.strategy
+  registryConfig$schema.name <- schema.name
+  registryConfig$schema.namespace <- schema.namespace
+  x %>%
+  spark_dataframe() %>%
+  invoke("select", list(invoke(invoke_static(sc, "org.apache.spark.sql.functions", "struct", head(invoke(., "columns"), 1)[[1]], tail(invoke(., "columns"), -1)), "as", "value"))) %>%
+  invoke("select", list(invoke(invoke_static(sc, "za.co.absa.abris.avro.functions", "to_confluent_avro", invoke_static(sc, "org.apache.spark.sql.functions", "col", "value"), writeRegistryConfig), "as", "value"))) %>% 
+  stream_write_kafka(options=list(kafka.bootstrap.servers = kafka.bootstrap.servers, topic = schema.registry.topic)) 
 }
