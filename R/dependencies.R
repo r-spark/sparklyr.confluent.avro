5a61da6f1607fc588046ecca98492e7af5981922
jeff perreton <jperreton@gmail.com>
2020-05-11 08:21:50 +0200

    remove scala code

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,10 +1,6 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
-    jars =
-      system.file(
-        "java/sparklyr.confluent.avro-2.4-5_2.11.jar",
-        package = "sparklyr.confluent.avro"
-      ),
+    jars =c(),
 	packages="io.confluent:kafka-schema-registry:5.4.1,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,org.apache.spark:spark-avro_2.11:2.4.5,za.co.absa:abris_2.11:3.1.1",
     repositories="http://packages.confluent.io/maven/"
   )
